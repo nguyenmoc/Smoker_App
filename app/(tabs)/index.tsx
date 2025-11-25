111c56a66f5ccca9ed73526000f5bad3470cf52e
tuhv <tuhv@gmail.com>
2025-11-25 23:14:59 +0700

    note minhtran

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -782,3 +782,26 @@ const styles = StyleSheet.create({
     fontWeight: '600',
   },
 });
+
+
+
+// <!-- Revive Adserver Asynchronous JS Tag - Generated with Revive Adserver v6.0.3 -->
+// <ins data-revive-zoneid="1" data-revive-id="53126d71827fcba70ff68055b9a73ca1"></ins>
+// <script async src="//localhost/revive/www/delivery/asyncjs.php"></script>
+
+
+// <!-- Revive Adserver Javascript Tag - Generated with Revive Adserver v6.0.3 -->
+// <script type='text/javascript'><!--//<![CDATA[
+//    var m3_u = (location.protocol=='https:'?'https://localhost/revive/www/delivery/ajs.php':'http://localhost/revive/www/delivery/ajs.php');
+//    var m3_r = Math.floor(Math.random()*99999999999);
+//    if (!document.MAX_used) document.MAX_used = ',';
+//    document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+//    document.write ("?zoneid=1");
+//    document.write ('&amp;cb=' + m3_r);
+//    if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+//    document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+//    document.write ("&amp;loc=" + encodeURIComponent(window.location));
+//    if (document.referrer) document.write ("&amp;referer=" + encodeURIComponent(document.referrer));
+//    if (document.context) document.write ("&context=" + encodeURIComponent(document.context));
+//    document.write ("'><\/scr"+"ipt>");
+// //]]>--></script><noscript><a href='https://localhost/revive/www/delivery/ck.php?n=a84ffcb4&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://localhost/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a84ffcb4' border='0' alt='' /></a></noscript>
